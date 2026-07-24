f271830d42480de5ace97218d39b3cccee7de581
DanielCohenn <dani1998c@gmail.com>
2026-07-25 01:14:01 +0300

    Added initial base class

--- src/cuda/gemm/gemmKernel.cuh
+++ src/cuda/gemm/gemmKernel.cuh
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 
 
 struct GemmMetrics {
@@ -9,18 +10,62 @@ struct GemmMetrics {
     std::string dtype;
 
     float timeMs;
+
+    bool isValid;
 };
 
 template <typename T>
 class GemmKernel {
 private:
     int m, n, k;
-public:
-    GemmKernel(int m, int n, int k) : m(m), n(n), k(k) {};
+    T* h_resReference;
 
-    GemmMetrics run(const T* h_matA, const T* h_matB, T* h_matC, T alpha, T beta);
+    void hostNaiveGemm(const T* h_matA, const T* h_matB, T alpha, T beta) {
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                T sum = 0;
+                for (int p = 0; p < k; ++p) {
+                    sum += h_matA[i * k + p] * h_matB[p * n + j];
+                }
+                h_resReference[i * n + j] = alpha * sum + beta * h_resReference[i * n + j];
+            }
+        }
+    }
+
+    bool validateGemm(const T* h_matC, const T* h_resReference) {
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (std::abs(h_matC[i * n + j] - h_resReference[i * n + j]) > 1e-3) {
+                    std::cerr << "Validation failed at (" << i << ", " << j << "): "
+                              << h_matC[i * n + j] << " != " << h_resReference[i * n + j] << std::endl;
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+public:
+    GemmKernel(int m, int n, int k) : m(m), n(n), k(k) {
+        h_resReference = new T[m * n];
+    };
+
+    GemmMetrics run(const T* h_matA, const T* h_matB, T* h_matC, T alpha, T beta) {
+        // allocate device mem
+
+        // h2d
+
+        // launch kernel
+
+        // d2h
+
+        // sync
+    }
 
     virtual void launch(const T* d_matA, const T* d_matB, T* d_matC, T alpha, T beta) = 0;
 
-    virtual ~GemmKernel() = default;
+    virtual ~GemmKernel() {
+        delete[] h_resReference;
+    }
 };
